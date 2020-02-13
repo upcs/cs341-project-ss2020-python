878fdb10ea2131875270e64347404875e3abed43
Kevin Hoser <hoser21@up.edu>
2020-02-13 11:59:27 -0800

    Add vuetify

--- app/vue.config.js
+++ app/vue.config.js
@@ -1,8 +1,11 @@
 module.exports = {
-  pluginOptions: {
-    express: {
-      shouldServeApp: true,
-      serverDir: './srv'
+  "pluginOptions": {
+    "express": {
+      "shouldServeApp": true,
+      "serverDir": "./srv"
     }
-  }
+  },
+  "transpileDependencies": [
+    "vuetify"
+  ]
 }
