539a3320699433efe9301db7b772925c9bfca022
Kevin Hoser <hoser21@up.edu>
2020-02-22 11:22:11 -0800

    Enabled express server on port 3000

--- app/srv/index.js
+++ app/srv/index.js
@@ -1,24 +1,15 @@
 import express from 'express';
-// import socketIO from "socket.io";
 
 export default (app, http) => {
-  // app.use(express.json());
-  //
-  // app.get('/foo', (req, res) => {
-  //   res.json({msg: 'foo'});
-  // });
-  //
-  // app.post('/bar', (req, res) => {
-  //   res.json(req.body);
-  // });
-  // 
-  // optional support for socket.io
-  // 
-  // let io = socketIO(http);
-  // io.on("connection", client => {
-  //   client.on("message", function(data) {
-  //     // do something
-  //   });
-  //   client.emit("message", "Welcome");
-  // });
+  app.use(express.json());
+
+  app.get('/foo', (req, res) => {
+    res.json({msg: 'foo'});
+  });
+  
+  app.post('/bar', (req, res) => {
+    res.json(req.body);
+  });
+  
+  app.listen(process.env.PORT);
 }
