55e78eceea435ef01ff26789586de947a162152f
Kevin Hoser <hoser21@up.edu>
2020-02-23 23:21:43 -0800

    Updated titlebar spacing

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,41 +1,45 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      absolute
-      color="primary"
-    >
-      <router-link to="/">
-        <v-img src="@/assets/images/logo_button.jpeg" max-width="250" max-heigh="50"></v-img>
-      </router-link>
+    <v-app-bar app color="primary">
+      <v-container fluid>
+        <v-row justify="center" align="center">
+          <v-col cols="2">
+            <router-link to="/">
+              <v-img src="@/assets/images/logo_button.jpeg" max-width="250" max-height="50"></v-img>
+            </router-link>
+          </v-col>
 
-      <v-spacer></v-spacer>
+          <v-col cols="4">
+            <router-link to="/demo">
+              <v-btn text>Demo</v-btn>
+            </router-link>
 
-      <router-link to="/demo">
-        <v-btn text>Demo</v-btn>
-      </router-link>
+            <router-link to="/news">
+              <v-btn text>News</v-btn>
+            </router-link>
 
-      <v-spacer></v-spacer>
-      
-      <router-link to="/news">
-        <v-btn text>News</v-btn>
-      </router-link>
+            <router-link to="/info">
+              <v-btn text>About Us</v-btn>
+            </router-link>
 
-      <v-spacer></v-spacer>
+            <router-link to="/contact">
+              <v-btn text>Contant Us</v-btn>
+            </router-link>
+          </v-col>
 
-      <router-link to="/info">
-        <v-btn text>About Us</v-btn>
-      </router-link>
-
-      <v-spacer></v-spacer>
-
-      <router-link to="/contact">
-        <v-btn text>Contant Us</v-btn>
-      </router-link>
-
-      <v-spacer></v-spacer>
-
-      <v-text-field label="Search" prepend-icon="mdi-magnify"></v-text-field>
+          <v-col cols="2" class="pt-7">
+            <v-text-field
+              label="Search"
+              prepend-inner-icon="mdi-magnify"
+              background-color="accent"
+              rounded
+              dense
+            ></v-text-field>
+          </v-col>
+          
+          <v-spacer></v-spacer>
+        </v-row>
+      </v-container>
     </v-app-bar>
 
     <v-content>
@@ -46,5 +50,5 @@
   </v-app>
 </template>
 
-<style lang="scss">
+<style>
 </style>
