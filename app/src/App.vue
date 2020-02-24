0dbc00729c41b2bffd38cdafeac3c9de74f5739e
Kevin Hoser <hoser21@up.edu>
2020-02-23 17:52:43 -0800

    Converted titlebar to vuetify elements

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,104 +1,50 @@
 <template>
-   <div id="app">
-    <div id="topnav">
-      <a><router-link to="/"><img border="0" alt="Home Page" src="@/assets/images/logo_button.jpeg" width="250" height="50"/></router-link></a>
-      <a id="sec_button"><router-link to="/demo">Demo</router-link></a>
-      <a id="sec_button"><router-link to="/news">News</router-link></a>
-      <a id="sec_button"><router-link to="/info">About Us</router-link></a>
-      <a id="sec_button"><router-link to="/contact">Contact Us</router-link></a>
-      <div class="searchbar">
-        <input type="text" placeholder="Search..." id="search-text-input" />
-        <div id="button-holder">
-          <img src="@/assets/images/mag_glass.png" />
-        </div>
-      </div>
-      <!-- <ul id="navBar">
-        <li><router-link to="/">Home</router-link></li>
-        <li><router-link to="/map">Map</router-link></li>
-        <li><router-link to="/info">Info</router-link></li>
-        <li><a href="#news">News</a></li>
-        <li><a href="#contact">Contact</a></li>
-        <li><a href="#about">About</a></li>
-      </ul> -->
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      absolute
+      color="primary"
+    >
+      <router-link to="/">
+        <v-img src="@/assets/images/logo_button.jpeg" max-width="250" max-heigh="50"></v-img>
+      </router-link>
+
+      <v-spacer></v-spacer>
+
+      <router-link to="/demo">
+        <v-btn text>Demo</v-btn>
+      </router-link>
+
+      <v-spacer></v-spacer>
+      
+      <router-link to="/news">
+        <v-btn text>News</v-btn>
+      </router-link>
+
+      <v-spacer></v-spacer>
+
+      <router-link to="/info">
+        <v-btn text>About Us</v-btn>
+      </router-link>
+
+      <v-spacer></v-spacer>
+
+      <router-link to="/contact">
+        <v-btn text>Contant Us</v-btn>
+      </router-link>
+
+      <v-spacer></v-spacer>
+
+      <v-text-field label="Search" prepend-icon="mdi-magnify"></v-text-field>
+    </v-app-bar>
+
+    <v-content>
+      <v-container fluid>
+        <router-view></router-view>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 
 <style lang="scss">
-  #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#topnav {
-  overflow: hidden;
-	background-color: #333;
-  a {
-    // font-weight: bold;
-    // color: #2c3e50;
-
-    // &.router-link-exact-active {
-    //   color: #42b983;
-    float: left;
-    color: #f2f2f2;
-    text-align: center;
-    padding: 14px 16px;
-    text-decoration: none;
-    font-size: 17px;
-    $font-family: Arial, Helvetica, sans-serif;
-    position: relative;
-    }
-  a:hover {
-    background-color: #ddd;
-    color: black;
-  }
-  a.active {
-    background-color: #4CAF50;
-    color: white;
-  }
-}
-#sec_button {
-  position: relative;
-  top: var(15px);
-}
-.searchbar {
-  position: relative;
-  top: var(20px);
-  left: var(5px);
-  text-decoration: none;
-  font-size: 17px;
-  $font-family: Arial, Helvetica, sans-serif;
-}
-#search-text-input{
-	border-top:thin solid  #e5e5e5;
-    border-right:thin solid #e5e5e5;
-    border-bottom:0;
-    border-left:thin solid  #e5e5e5;
-    box-shadow:0px 1px 1px 1px #e5e5e5;
-    float:left;
-    height: var(20px);
-    margin:auto; 
-    outline:0;
-    padding:.4em 0 .4em .6em; 
-    width: var(183px);
-}
-#button-holder{
-    background-color:var(#f1f1f1);
-    border-top:thin solid #e5e5e5;
-    box-shadow:1px 1px 1px 1px #e5e5e5;
-    cursor:pointer;
-    float:left;
-    height: var(27.5px);
-    margin: auto;
-    text-align:center;
-    width: var(50px);
-}
-#button-holder img{
-    margin: var(4px);
-    width: var(20px); 
-}
 </style>
--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -1,25 +1,7 @@
 <template>
   <div class="background">
     <div class="topnav"></div>
-      <!-- <a href="cs341-project-ss2020-python/app/public/index.html">
-        <img border="0" alt="Home Page" src="@/assets/images/logo_button.jpeg" width="250" height="50" />
-      </a> -->
-      <!-- <a href="#demo" id="sec_button">Demo</a> -->
-      <!-- <a href="#news" id="sec_button">News</a>
-      <a href="#about" id="sec_button">About Us</a>
-      <a href="#contact" id="sec_button">Contact Us</a>
-      <div class="searchbar">
-        <input type="text" placeholder="Search..." id="search-text-input" />
-        <div id="button-holder">
-          <img src="@/assets/images/mag_glass.png" />
-        </div>
-      </div> -->
-    <!-- insert image here -->
-
-    <!--<textarea rows="1" cols="100" placeholder="Search..."></textarea>-->
-    <!-- https://www.codeofaninja.com/2014/03/textbox-with-search-icon-in-html-css.html -->
-
-    <div class="container">
+     <div class="container">
       <img id="bg" src="@/assets/images/background.jpeg"/>
       <img id="logo" src="@/assets/images/logo.png"/>
     </div>
@@ -33,29 +15,29 @@ export default {
 </script>
 
 <style lang="scss">
-.background{
-  background-color: #333;
-  text-align: center;
-  margin: var(0);
-  height: 100%;
-  width: auto;
-  background-repeat: no-repeat;
-}
-.container{
-  position: relative;
-}
-#bg{
-  height: 100%;
-  width: 100%;
-  background-position: center;
-  margin: 0;
-  position: relative;
-}
-#logo{
-  position: relative;
-  align-items: center;
-  margin-top: var(-650px);
-  width: var(500px);
-  height: var(50px);
-}
+// .background{
+//   background-color: #333;
+//   text-align: center;
+//   margin: var(0);
+//   height: 100%;
+//   width: auto;
+//   background-repeat: no-repeat;
+// }
+// .container{
+//   position: relative;
+// }
+// #bg{
+//   height: 100%;
+//   width: 100%;
+//   background-position: center;
+//   margin: 0;
+//   position: relative;
+// }
+// #logo{
+//   position: relative;
+//   align-items: center;
+//   margin-top: var(-650px);
+//   width: var(500px);
+//   height: var(50px);
+// }
 </style>
