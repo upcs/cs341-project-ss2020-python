0c64625d208ec11349fb8ec329c30000a1ec2718
argiesemann <gieseman21@up.edu>
2020-02-24 16:57:30 -0800

    reduced scope of unit test

--- app/tests/unit/dbquery.spec.js
+++ app/tests/unit/dbquery.spec.js
@@ -1,21 +1,22 @@
 /**
- * ensures that dbms and sqlMidWare are functioning as expected
- * that is, we send a get to sqlMidWare, where the get request exists just for testing
- * and that get sends a default query to the database. We're checking that first value returned
- * contains the expected values
+ * ensures that dbms queries are functioning
  */
 
-const $ = require('jquery');
+var dbms = require('../../srv/javascript/dbms');
+
 
 function getDefaultData() {
-	$.get('http://localhost:3000/sqlMidWare', function (response) {
-		console.log(response[0].BACODE)
-		return response[0].BACODE;
-	};
+    return new Promise((resolve, reject) => {
+        dbms.dbquery('select * from PlantEmissions2018 where PLPRMFL=\'NUC\';', function (error, result) {
+
+            console.log("database got queried");
+            resolve(result[0].BACODE);
+
+        });
+    });
+	
 }
 
-test('test GET for /sqlMidWare', () => {
-	return getDefaultData().then(data => {
-		expect(data).toBe('TVA');
-	});
+test('test dbquery for /dbms', () => {
+    return expect(getDefaultData()).resolves.toEqual('TVA');
 });
