2ccf5fe405a1e11b51d89f37b9c00faf40ae4e6f
marcial21 <45084026+marcial21@users.noreply.github.com>
2020-02-26 20:25:09 -0800

    Started implementaton of contact us page
    Also finished up prototype 1 for about us page.

--- app/src/views/AboutUs.vue
+++ app/src/views/AboutUs.vue
@@ -11,23 +11,22 @@
                 </div>
                 <div class="v-parallax__content">
                     <div class="layout white--text column align-center justify-center">
-                        <h1 class="white--text mb-2 display-4 text-center">About Us</h1>
+                        <h1 class="white--text mb-2 display-4 text-center font-weight-bold">About Us</h1>
                     </div>
                 </div>
             </div>
         </section>
         <v-row justify="center" align="center">
-            <img src="@/assets/images/oilPump.png">
+            <img src="@/assets/images/oilPump.png" class="">
             <div class="flex xs12 sm4">
-                <div class="transparent v-card v-card--flat v-sheet theme--light">
-                <v-card-text class="subtitle-1">
-                    
+                <div class="transparent v-card v-card--flat v-sheet theme--light pr-12">
+                <v-card-text class="title text-justify">
                     This web application was designed by a group of software engineers that have a vision of a world that uses clean methods of energy production. As we acknowledge the state of the world we live in, it is important to pursue change as quickly as possible. Pollution, climate change, and greenhouse gases are just a few negative effects that old school energy production methods have. These include burning fossil fuels, solar panels, hydroelectric energy, nuclear energy and many more. As a group of software engineers our goal is to be able to collect current data on the status of energy production and their impact on the environment via different cities. We hope to make this web application that may educate people on the importance of the environment and gain support for using clean methods of energy production in order to undo the damage to our planet. Join us and pursue this future with us!
-                    
+
                 </v-card-text>
                 </div>
             </div>
-            <img src="@/assets/images/cartoonEnergy.png">
+            <img src="@/assets/images/cartoonEnergy.png" class="pl-12">
             <img src="@/assets/images/future.jpg">
         </v-row>
     </div>
