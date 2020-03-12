66e317d12572ff4ef25822264777b423e447bc43
marcial21 <45084026+marcial21@users.noreply.github.com>
2020-03-11 20:23:01 -0700

    updates
    renamed sum.test to sum.spec,
    changed the route,
    and added the sum.js

--- app/tests/unit/sum.spec.js
+++ app/tests/unit/sum.spec.js
@@ -1,8 +1,9 @@
 /**
  * Just the sum.test.js
  */
+import { shallowMount } from '@vue/test-utils'
 
-const sum = require('../public/javascripts/sum.js');
+const sum = require('../../srv/javascript/sum');
 	test('adds 1 + 2 to equal 3', () => {
 		expect(sum(1,2)).toBe(3);
 	});
