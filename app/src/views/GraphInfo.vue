3da5ba53538dda23525b05c4a7b15166cb952f9f
smokeburned <smokeburned@gmail.com>
2020-03-18 20:49:24 -0700

    Implemented Error tags for graph view inputs
    city1, city2 (when enabled) and plant type all have error checking to make sure they are not null before the request is made

--- app/src/views/GraphInfo.vue
+++ app/src/views/GraphInfo.vue
@@ -37,6 +37,9 @@
                         <v-text-field label="City 1"
                                       placeholder="Portland"
                                       v-model="city"></v-text-field>
+                        <div v-if="errors.city1 === true">
+                            <error message="Please Enter a City"> </error>
+                        </div>
                     </v-row>
 
                     <v-row>
@@ -46,7 +49,11 @@
                             ></v-checkbox>
                         <v-text-field v-if="secondCity" label="City 2"
                                       placeholder="Portland"
-                                      v-model="city2"></v-text-field>
+                                      v-model="city2">
+                                      </v-text-field>
+                        <div v-if="errors.city2 === true">
+                            <error message="Please Enter a City"> </error>
+                        </div>
                     </v-row>
 
                     <v-row>
@@ -55,7 +62,9 @@
                                   v-model="plant"
                                   label="Plant Type"
                                   :multiple="true"></v-select>
-
+                        <div v-if="errors.plant === true">
+                            <error message="Please Select at least 1 Plant Type"> </error>
+                        </div>
                     </v-row>
 
                 </v-row>
@@ -103,20 +112,27 @@
 
 <script>
   import Chart from "@/components/Chart.vue"
+  import Error from "@/views/Error.vue"
   export default {
     name: 'GraphInfo', 
     components: {
         Chart,
+        Error,
     },
     data () {
         return {
             min: 0,
             max: 1000,
             slider: 100,
-            plant: null,
+            plant: [],
             city: null,
             city2: null,
             secondCity: false,
+            errors: {
+                'city1': false,
+                'city2': false,
+                'plant': false
+            },
             items: [
                 'Nuclear',
                 'Coal',
@@ -151,58 +167,76 @@
             this.loadChart = false;
             
             var chart = this;
-
+            if(this.city == null)
+                this.errors.city1 = true;
+            else
+                this.errors.city1 = false;
+            if(this.city2 == null && this.secondCity == true)
+                this.errors.city2 = true;
+            else
+                this.errors.city2 = false;
+            if(this.plant.length == 0) 
+                this.errors.plant = true;
+            else
+                this.errors.plant = false;
             var form = window.$("form");
-            console.log(form[0].elements);
-            
-
+            console.log(form[0].elements);  
             //first, use this resource to find out the lat and lon of the input city
-            window.$.get('https://nominatim.openstreetmap.org/search?q=' + this.city + '&format=json', function (cityData) {
+            if(this.errors.city1 && this.errors.city2 && this.errors.plant) {
+                window.$.get('https://nominatim.openstreetmap.org/search?q=' + this.city + '&format=json', function (cityData) {
 
-                //console.log(cityData);
+                    //console.log(cityData);
 
-                //if no cities were returned, don't go further. The user probably misspelled something
-                if (cityData.length > 0) { 
+                    //if no cities were returned, don't go further. The user probably misspelled something
+                    if (cityData.length > 0) { 
 
-                    var lat = cityData[0].lat;
-                    var long = cityData[0].lon;
+                        var lat = cityData[0].lat;
+                        var long = cityData[0].lon;
 
-                    window.$.post('http://localhost:3000/sqlMidWare', {
-                        distance: this.slider,
-                        'city': this.city,
-                        //'city2': this.city2,
-                        'plant': this.plant,
-                        longitude: long,
-                        latitude: lat,
-                        //'emissions': this.selectedData,
-                        //'energy': this.selectedEnergy,
-                    }, function (responseData) {
+                        window.$.post('http://localhost:3000/sqlMidWare', {
+                            distance: this.slider,
+                            'city': this.city,
+                            //'city2': this.city2,
+                            'plant': this.plant,
+                            longitude: long,
+                            latitude: lat,
+                            //'emissions': this.selectedData,
+                            //'energy': this.selectedEnergy,
+                        }, function (responseData) {
 
+                            
+                            console.log(responseData[0].avgCO2);
+                            var resData = parseFloat(responseData[0].avgCO2);
+                            chart.chart_data = {
+
+                                labels: [this.plant],
+
+                                datasets: [{
+                                    label: this.plant,
+                                    backgroundColor: "#f87979",
+                                    data: [resData]
+                                }]
+
+                            };
+                            console.log(chart.chart_data);
+                            chart.loadChart = true;
+                            
                         
                         console.log(responseData[0].avgCO2);
-                        var resData = parseFloat(responseData[0].avgCO2);
-                        chart.chart_data = {
 
                             labels: [this.plant],
 
                             datasets: [{
                                 label: this.plant,
                                 backgroundColor: "#f87979",
-                                data: [resData]
-                            }]
 
-                        };
-                        console.log(chart.chart_data);
-                        chart.loadChart = true;
-                        
-                     
-
+                        }
+                        );
                     }
-                    );
-                }
-            });
+                });
 
-            console.log(this.chart_data);
+                console.log(this.chart_data);
+            }
         } 
     }
   }
