788b78d17f73ac4eb47e2815ca26085d7146247f
Kevin Hoser <hoser21@up.edu>
2020-03-31 14:53:06 -0700

    Add tabs

--- travis_decrypt.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -ev
-
-openssl aes-256-cbc -K $encrypted_aedd9205a433_key -iv $encrypted_aedd9205a433_iv
--in python-portland-pollution-fdbfcd931162.json.enc -out python-portland-pollution-fdbfcd931162.json
--d;
