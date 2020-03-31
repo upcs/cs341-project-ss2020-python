7922880cd33bc5ca5e1adda67cf7dc90dcdd168b
Kevin Hoser <hoser21@up.edu>
2020-03-31 14:18:46 -0700

    Changed if conditions to travis.yml

--- travis_decrypt.sh
+++ travis_decrypt.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -ev
 
-if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
-    openssl aes-256-cbc -K $encrypted_aedd9205a433_key -iv $encrypted_aedd9205a433_iv
-    -in python-portland-pollution-fdbfcd931162.json.enc -out python-portland-pollution-fdbfcd931162.json
-    -d;
-fi
+openssl aes-256-cbc -K $encrypted_aedd9205a433_key -iv $encrypted_aedd9205a433_iv
+-in python-portland-pollution-fdbfcd931162.json.enc -out python-portland-pollution-fdbfcd931162.json
+-d;
