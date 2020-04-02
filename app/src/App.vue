2b26a6dcb045a12bbfa1f84b4e5406dbffa44a96
Jordan Ho <hojo21@up.edu>
2020-04-01 18:59:46 -0700

    commiting merge changes, but need to fix demo

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,5 +1,4 @@
 <template>
-<<<<<<< HEAD
     <v-app>
       <v-app-bar app color="primary">
         <v-container fluid>
@@ -24,10 +23,6 @@
 
               <router-link to="/info">
                 <v-btn text id="header">Energy Info</v-btn>
-              </router-link>
-
-              <router-link to="/about">
-                <v-btn text id="header">About Us</v-btn>
               </router-link>
 
               <router-link to="/contact">
@@ -78,53 +73,6 @@
         </v-container>
       </v-content>
     </v-app>
-=======
-  <v-app>
-    <v-app-bar app color="primary">
-      <v-container fluid>
-        <v-row justify="center" align="center">
-          <v-col cols="2">
-            <router-link to="/">
-              <v-img src="@/assets/images/logo_button.jpeg" max-width="250" max-height="50"></v-img>
-            </router-link>
-          </v-col>
-
-          <v-col cols="5">
-            <router-link to="/graph">
-              <v-btn text id="header">Plant Graphs</v-btn>
-            </router-link>
-
-            <router-link to="/news">
-              <v-btn text id="header">News</v-btn>
-            </router-link>
-
-            <router-link to="/contact">
-              <v-btn text id="header">Contact Us</v-btn>
-            </router-link>
-          </v-col>
-
-          <v-col cols="2" class="pt-7">
-            <v-text-field
-              label="Search"
-              prepend-inner-icon="mdi-magnify"
-              background-color="accent"
-              rounded
-              dense
-            ></v-text-field>
-          </v-col>
-          
-          <v-spacer></v-spacer>
-        </v-row>
-      </v-container>
-    </v-app-bar>
-
-    <v-content>
-      <v-container fluid>
-        <router-view></router-view>
-      </v-container>
-    </v-content>
-  </v-app>
->>>>>>> master
 </template>
 
 <style>
