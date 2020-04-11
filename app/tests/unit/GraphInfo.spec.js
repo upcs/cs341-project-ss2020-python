e91b1ef115661fdc53429b3bc19984140baf5f39
hoser21 <hoser21@up.edu>
2020-04-11 16:16:18 -0700

    Added unit tests for error checking

--- app/tests/unit/GraphInfo.spec.js
+++ app/tests/unit/GraphInfo.spec.js
@@ -68,15 +68,17 @@ describe('GraphInfo', () => {
   })
 
   it('testing the formPost function', async () => {
-    var latsAndLongs = await wrapper.vm.cityInfoGetter(['Portland']);
-
     wrapper = mount(GraphInfo, {
       data() {
         return {
           slider: 100,
           plant: ['Coal'],
           city: 'Portland',
-          selectedData: 'CO2 Emission Rate (lb/MWh)'
+          city2: 'Seattle',
+          secondCity: true,
+          sortBy: 'plant',
+          selectedData: 'CO2 Emission Rate (lb/MWh)',
+          selectedData2: 'Annual Net Power (MWh)'
         }
       },
       created() {
@@ -91,8 +93,137 @@ describe('GraphInfo', () => {
     await wrapper.vm.formPost() 
     //formPost should log finished chart_data local var
     expect(wrapper.vm.chart_data.datasets[0].label).toBe("Portland-CO2 Emission Rate (lb/MWh)");
+    expect(wrapper.vm.chart_data.datasets[1].label).toBe("Portland-Annual Net Power (MWh)");
+    expect(wrapper.vm.chart_data.datasets[2].label).toBe("Seattle-CO2 Emission Rate (lb/MWh)");
+    expect(wrapper.vm.chart_data.datasets[3].label).toBe("Seattle-Annual Net Power (MWh)");
     expect(wrapper.vm.chart_data.datasets[0].yAxisID).toBe("CO2 Emission Rate (lb/MWh)");
+    expect(wrapper.vm.chart_data.datasets[1].yAxisID).toBe("Annual Net Power (MWh)");
+    expect(wrapper.vm.chart_data.datasets[2].yAxisID).toBe("CO2 Emission Rate (lb/MWh)");
+    expect(wrapper.vm.chart_data.datasets[3].yAxisID).toBe("Annual Net Power (MWh)");
     expect(wrapper.vm.chart_data.datasets[0].stack).toBe("0");
+    expect(wrapper.vm.chart_data.datasets[1].stack).toBe("1");
+    expect(wrapper.vm.chart_data.datasets[2].stack).toBe("0");
+    expect(wrapper.vm.chart_data.datasets[3].stack).toBe("1");
     expect(wrapper.vm.chart_data.datasets[0].data).toStrictEqual([2484.322]);
+    expect(wrapper.vm.chart_data.datasets[1].data).toStrictEqual([5367383]);
+    expect(wrapper.vm.chart_data.datasets[2].data).toStrictEqual([2484.322]);
+    expect(wrapper.vm.chart_data.datasets[3].data).toStrictEqual([5367383]);
+  })
+
+  it ('test formatChartByCity', async () => {
+    wrapper = mount(GraphInfo, {
+      data() {
+        return {
+          slider: 100,
+          plant: ['Coal'],
+          city: 'Portland',
+          city2: 'Seattle',
+          selectedData: 'CO2 Emission Rate (lb/MWh)',
+          selectedData2: 'Annual Net Power (MWh)',
+          sortBy: 'city',
+          secondCity: true,
+        }
+      },
+      created() {
+          this.$vuetify.lang = {
+            t: () => {},
+          };
+        this.$vuetify.theme = { dark: false };
+        this.$vuetify.slots = { scopedSlots: ''}
+      }
+    });
+
+    await wrapper.vm.formPost();
+    
+    expect(wrapper.vm.chart_data.labels).toStrictEqual(["Portland", "Seattle"]);
+    expect(wrapper.vm.chart_data.datasets[0].yAxisID).toBe("CO2 Emission Rate (lb/MWh)");
+    expect(wrapper.vm.chart_data.datasets[1].yAxisID).toBe("Annual Net Power (MWh)");
+    expect(wrapper.vm.chart_data.datasets[0].label).toBe("Coal-CO2 Emission Rate (lb/MWh)");
+    expect(wrapper.vm.chart_data.datasets[1].label).toBe("Coal-Annual Net Power (MWh)");
+    expect(wrapper.vm.chart_data.datasets[0].stack).toBe(0);
+    expect(wrapper.vm.chart_data.datasets[1].stack).toBe(1);
+    expect(wrapper.vm.chart_data.datasets[0].data).toStrictEqual([2484.322, 2484.322]);
+    expect(wrapper.vm.chart_data.datasets[1].data).toStrictEqual([5367383, 5367383]);
+  })
+
+  it ('tests formPost error handling (invalid city)', async () => {
+    wrapper = mount(GraphInfo, {
+      data() {
+        return {
+          slider: 100,
+          plant: ['Coal'],
+          city: 'InvalidCity',
+          city2: 'AlsoInvalid',
+          selectedData: 'CO2 Emission Rate (lb/MWh)',
+          secondCity: true
+        }
+      },
+      created() {
+          this.$vuetify.lang = {
+            t: () => {},
+          };
+        this.$vuetify.theme = { dark: false };
+        this.$vuetify.slots = { scopedSlots: ''}
+      }
+    });
+
+    await wrapper.vm.formPost();
+
+    expect(wrapper.find('#city1ErrNoResults').exists()).toBe(true)
+    expect(wrapper.find('#city2ErrNoResults').exists()).toBe(true)
+    expect(wrapper.vm.chart_data).toBeNull();
+  })
+
+  it ('tests formPost error handling (duplicate city)', async () => {
+    wrapper = mount(GraphInfo, {
+      data() {
+        return {
+          slider: 100,
+          plant: ['Coal'],
+          city: 'Portland',
+          city2: 'Portland',
+          secondCity: true,
+          selectedData: 'CO2 Emission Rate (lb/MWh)',
+        }
+      },
+      created() {
+          this.$vuetify.lang = {
+            t: () => {},
+          };
+        this.$vuetify.theme = { dark: false };
+        this.$vuetify.slots = { scopedSlots: ''}
+      }
+    });
+
+    await wrapper.vm.formPost();
+
+    expect(wrapper.find('#city2ErrDuplicate').exists()).toBe(true);
+    expect(wrapper.vm.chart_data.datasets.length).toBe(1);
+  })
+
+  it ('tests formPost error handling (duplicate paramemter)', async () => {
+    wrapper = mount(GraphInfo, {
+      data() {
+        return {
+          slider: 100,
+          plant: ['Coal'],
+          city: 'Portland',
+          selectedData: 'CO2 Emission Rate (lb/MWh)',
+          selectedData2: 'CO2 Emission Rate (lb/MWh)'
+        }
+      },
+      created() {
+          this.$vuetify.lang = {
+            t: () => {},
+          };
+        this.$vuetify.theme = { dark: false };
+        this.$vuetify.slots = { scopedSlots: ''}
+      }
+    });
+
+    await wrapper.vm.formPost() 
+
+    expect(wrapper.find('#param2Err').exists()).toBe(true);
+    expect(wrapper.vm.chart_data.datasets.length).toBe(1);
   })
 })
