66689966b911735c354b7d8ef9a7c87d1d34da2c
Andres Giesemann <andresrgiesemann@gmail.com>
2020-04-14 12:57:18 -0700

    Improved color contrast.
    copied a method from stack overflow for color darkening (with citation)

--- app/src/views/GraphInfo.vue
+++ app/src/views/GraphInfo.vue
@@ -272,9 +272,54 @@
                 }
             }
 
+            var seenColors = [];
+
+            //go back over and darken any repeated colors for easier visibility
+            for (var dataset of newChartData['datasets']) {
+
+                    if (seenColors.includes(dataset['backgroundColor'])) {
+                        seenColors.push(dataset['backgroundColor']);
+                        dataset['backgroundColor'] = chart.LightenDarkenColor(dataset['backgroundColor'], -20);
+                    }
+                    else {
+                        seenColors.push(dataset['backgroundColor']);
+                    }
+            }
             chart.chart_data = newChartData;
 
             chart.loadChart = true;
+        },
+
+        /**
+         * LightenDarkenColor
+         * 
+         * Source: https://stackoverflow.com/questions/5560248/programmatically-lighten-or-darken-a-hex-color-or-rgb-and-blend-colors
+         */
+        LightenDarkenColor: function(col,amt) {
+            var usePound = false;
+            if ( col[0] == "#" ) {
+                col = col.slice(1);
+                usePound = true;
+            }
+
+            var num = parseInt(col,16);
+
+            var r = (num >> 16) + amt;
+
+            if ( r > 255 ) r = 255;
+            else if  (r < 0) r = 0;
+
+            var b = ((num >> 8) & 0x00FF) + amt;
+
+            if ( b > 255 ) b = 255;
+            else if  (b < 0) b = 0;
+
+            var g = (num & 0x0000FF) + amt;
+
+            if ( g > 255 ) g = 255;
+            else if  ( g < 0 ) g = 0;
+
+            return (usePound?"#":"") + (g | (b << 8) | (r << 16)).toString(16);
         },
 
         /**
