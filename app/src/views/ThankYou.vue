7b623517b6f331cd7d4a6fa846b6aff6234e5410
marcial21 <45084026+marcial21@users.noreply.github.com>
2020-04-21 10:42:47 -0700

    Update ThankYou.vue

--- app/src/views/ThankYou.vue
+++ app/src/views/ThankYou.vue
@@ -14,75 +14,63 @@
                           
                             <div id="ContactUs" class="font-weight-light" style="font-size: 30px;">Thank you!</div>
                             <div class="subtitle-1 font-weight-light">Your form has been submitted.</div>
-                            
-                
-                          
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </section>
-            <section class="container" id="subscribe" style="padding: 0px 200px 0px;">
-              <div class="pa-3 v-card v-card--flat v-sheet v-sheet--title theme--light"
-                style="background-color: rgba(7, 150, 245, 1.0); border-color: white;">
-                <div class="container grid-list-x1 pa-0">
-                  <div class="layout wrap">
-                    <div class="flex xs12 md6 offset-md1">
-                      <h2 class="subheading white--text text-uppercase font-weight-medium mb-3">
-                        Contact
-                      </h2>
-                      <div role="list" class="v-list transparent theme--light">
-                        <div role="listitem">
-                          <div class="v-list__tile theme--light">
-                            <div class="v-list__tile__avatar">
-                              <div class="v-avatar primary" style="height: 40px;
-                              width: 40px;">
-                                <i aria-hidden="true" class="v-icon elevation-4
-                                mdi mdi-email-outline theme--light">
+                            <div class="py-2"></div>
+              
+                            <div class="flex xs12 md6" style = "padding: 0px 10px 0px;">
+                              <h2 class="subheading white--text text-uppercase font-weight-medium mb-3">
+                                Contact
+                              </h2>
+                              <div role="list" class="v-list transparent theme--light">
+                                <div role="listitem">
+                                  <div class="v-list__tile theme--light">
+                                    <div class="v-list__tile__avatar">
+                                      <div class="v-avatar primary" style="height: 40px;
+                                      width: 40px;">
+                                        <i aria-hidden="true" class="v-icon elevation-4
+                                        mdi mdi-email-outline theme--light">
+                                          
+                                        </i>
+                                      </div>
+                                      <v-text class="white--text" style="padding: 0px 10px 0px;">PortlandPythonPollution@PPP.com</v-text>
+                                    </div>
+
+                                  </div>
                                   
-                                </i>
-                              </div>
-                            </div>
-                            <div class="v-list__tile__title white--text">EMAIL</div>
-                            <div class="v-list__tile__sub-title white--text">
-                              PortlandPythonPollution@gmail.com</div>
-
-                          </div>
-                        </div>
-                        <div role="listitem">
-                          <div class="v-list__tile theme--light">
-                            <div class="v-list__tile__avatar">
-                              <div class="v-avatar primary" style="height: 40px;
-                              width: 40px;">
-                                <i aria-hidden="true" class="v-icon elevation-4
-                                mdi mdi-map-marker theme--light">
+                                  <div class="py-2"></div>
+                                </div>
+                                <div role="listitem">
+                                  <div class="v-list__tile theme--light">
+                                    <div class="v-list__tile__avatar">
+                                      <div class="v-avatar primary" style="height: 40px;
+                                      width: 40px;">
+                                        <i aria-hidden="true" class="v-icon elevation-4
+                                        mdi mdi-map-marker theme--light">
+                                          
+                                        </i>
+                                      </div>
+                                      <v-text class="white--text" style="padding: 0px 10px 0px;">North Portland</v-text>
+                                    </div>
                                   
-                                </i>
+                                    <div class="py-2"></div>
+                                  </div>
+                                </div>
+                                <div role="listitem">
+                                  <div class="v-list__tile theme--light">
+                                    <div class="v-list__tile__avatar text--white">
+                                      <div class="v-avatar primary" style="height: 40px;
+                                      width: 40px;">
+                                        <i aria-hidden="true" class="v-icon elevation-4
+                                        mdi mdi-phone theme--light">
+                                        </i>
+                                      </div>
+                                      <v-text class="white--text" style="padding: 0px 10px 0px;">877-PPP-NOW</v-text>
+                                    </div>
+                                    
+                                    <div class="py-2"></div>
+                                  </div>
+                                </div>
                               </div>
-                            </div>
-                            <div class="v-list__tile__title white--text">ADDRESS</div>
-                            <div class="v-list__tile__sub-title white--text">
-                              ISLAND OF NUNYA</div>
-
-                          </div>
-                        </div>
-                        <div role="listitem">
-                          <div class="v-list__tile theme--light">
-                            <div class="v-list__tile__avatar">
-                              <div class="v-avatar primary" style="height: 40px;
-                              width: 40px;">
-                                <i aria-hidden="true" class="v-icon elevation-4
-                                mdi mdi-phone theme--light">
-                                </i>
-                              </div>
-                            </div>
-                            <div class="v-list__tile__title white--text">PHONE</div>
-                            <div class="v-list__tile__sub-title white--text">
-                              (123) 456-7890</div>
-
-                          </div>
+                            </div>    
                         </div>
                       </div>
                     </div>
